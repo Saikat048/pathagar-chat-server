04a19b434d3ea3ed60d03b2fe78576b0bb427798
Saikat048 <mdsaikathosan048@gmail.com>
2022-12-06 21:31:53 +0600

    commit client live link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://pathagar-7e2af.web.app",
     methods: ["GET", "POST"],
   },
 });
